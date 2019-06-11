59d425cd2202eb6370dd769b5bb78b3d1fdd4d84
Huw Griffiths <huw.m.griffiths@gmail.com>
2019-06-12 08:21:44 +1000

    Add Children Component
    I need Child relationships to implement a Font component that will
    construct a whole bunch of other Entities and need to destroy them when
    it is removed or updated.

--- ecs/parent.go
+++ ecs/parent.go
@@ -14,7 +14,9 @@ func (*Parent) Type() string {
 // ParentSystem manages Parent Components.
 type ParentSystem struct{}
 
+// Update the ParentSystem.
 func (s *ParentSystem) Update(mgr *World) {
+	// Remove orphans.
 	for _, e := range mgr.Get([]string{"Parent"}) {
 		p := mgr.Component(e, "Parent").(*Parent)
 
@@ -24,4 +26,28 @@ func (s *ParentSystem) Update(mgr *World) {
 
 		mgr.DestroyEntity(e)
 	}
+
+	// Remove references to Children that have been destroyed from the Parents
+	// that claim them.
+	for _, e := range mgr.Get([]string{"Children"}) {
+		c := mgr.Component(e, "Children").(*Children)
+		val := []Entity{}
+		for _, child := range c.Value {
+			if !mgr.Exists(child) {
+				continue
+			}
+			val = append(val, child)
+		}
+		c.Value = val
+	}
+}
+
+// Children allows hierarchical relationships between entities. Some Entities have Children.
+type Children struct {
+	Value []Entity
+}
+
+// Type of this Component.
+func (*Children) Type() string {
+	return "Children"
 }
