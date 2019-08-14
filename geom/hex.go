40bb4379ae2d9fdf820260e20e34fef8338b74a8
Huw Griffiths <huw.m.griffiths@gmail.com>
2019-08-14 19:45:31 +1000

    Concatenate all Hex types into one file
    These were really small files, and since I put all the Field adapters
    into field.go, these looked like they belonged in the same file too.

--- geom/hex.go
+++ geom/hex.go
@@ -36,3 +36,67 @@ func (h *Hex) Key() Key {
 func (h *Hex) Hexes() []*Hex {
 	return []*Hex{h}
 }
+
+// Hex4 represents four hexes that can be occupied by a medium-sized unit.
+type Hex4 struct {
+	M, N      int // corresponds to the northern Hex.
+	hexes     map[DirectionType]*Hex
+	neighbors []*Hex4
+}
+
+// X coordinate of the center of this hexagon.
+func (h *Hex4) X() float64 {
+	oddXOffset := xStride / 2
+	return (hexWidth / 2) + float64((xStride*h.M)+(h.N%2*oddXOffset))
+}
+
+// Y coordinate of the center of this hexagon.
+func (h *Hex4) Y() float64 {
+	return yStride + float64(yStride*h.N) + yStride
+}
+
+// Key returns the M,N coordinates of this Hex4.
+func (h *Hex4) Key() Key {
+	return Key{h.M, h.N}
+}
+
+// Hexes returns the Hexes that compose this Hex4.
+func (h *Hex4) Hexes() []*Hex {
+	var result []*Hex
+	for _, hex := range h.hexes {
+		result = append(result, hex)
+	}
+	return result
+}
+
+// Hex7 represents a Hexagon and the surrounding 6 hexagons.
+type Hex7 struct {
+	M, N      int // corresponds to the center Hex.
+	hexes     map[DirectionType]*Hex
+	neighbors []*Hex7
+}
+
+// X coordinate of the center of this hexagon.
+func (h *Hex7) X() float64 {
+	oddXOffset := xStride / 2
+	return (hexWidth / 2) + float64((xStride*h.M)+(h.N%2*oddXOffset))
+}
+
+// Y coordinate of the center of this hexagon.
+func (h *Hex7) Y() float64 {
+	return yStride + float64(yStride*h.N)
+}
+
+// Key returns the M,N coordinates of this Hex7.
+func (h *Hex7) Key() Key {
+	return Key{h.M, h.N}
+}
+
+// Hexes returns the Hexes that compose this Hex7.
+func (h *Hex7) Hexes() []*Hex {
+	var result []*Hex
+	for _, hex := range h.hexes {
+		result = append(result, hex)
+	}
+	return result
+}
