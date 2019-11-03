72e0e01e2b65d52645a30998b5ec763cb65fac96
Huw Griffiths <huw.m.griffiths@gmail.com>
2019-11-03 20:13:44 +1100

    Fix X-coordinate calculation of hexes
    There was a bug in 43edc205035c3b328654a5d6a0b7835eae9d07c1 that
    mis-aligned the hex selection for combat.

--- geom/hex_test.go
+++ geom/hex_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestHexXY(t *testing.T) {
-
 	tests := []struct {
 		m, n int
 		x, y float64
@@ -17,18 +16,14 @@ func TestHexXY(t *testing.T) {
 		{1, 3, 63, 32},
 	}
 	for _, tc := range tests {
-		t.Run(fmt.Sprintf("%d-%d_%f-%f", tc.m, tc.n, tc.x, tc.y), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d,%d", tc.m, tc.n), func(t *testing.T) {
 			h := Hex{
 				M: tc.m,
 				N: tc.n,
 			}
 
-			if h.X() != tc.x {
-				t.Errorf("want %f got %f", tc.x, h.X())
-			}
-
-			if h.Y() != tc.y {
-				t.Errorf("want %f got %f", tc.y, h.Y())
+			if h.X() != tc.x || h.Y() != tc.y {
+				t.Errorf("want %f,%f got %f,%f", tc.x, tc.y, h.X(), h.Y())
 			}
 		})
 	}
