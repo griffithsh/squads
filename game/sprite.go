e20e9349c52e857e632401cc9c67fdba2a417af2
Huw Griffiths <huw.m.griffiths@gmail.com>
2020-01-26 20:02:56 +1100

    Document Sprite Offsets a little better

--- game/sprite.go
+++ game/sprite.go
@@ -2,8 +2,13 @@ package game
 
 // Sprite is a renderable slice of a texture.
 type Sprite struct {
-	Texture          string
-	X, Y, W, H       int
+	Texture    string
+	X, Y, W, H int
+
+	// OffsetX and OffsetY exist as they can be applied to individual frames of
+	// an animation. They do not duplicate the functionality of RenderOffset, as
+	// that applies at the Entity level, and could effect the rendering position
+	// of things that are not Sprites. (i.e shape primitives)
 	OffsetX, OffsetY int
 
 	// This could include a color, but does not for now, as there are no uses for it.
