83f567c182c0744cc96fd6e9fb2e217fe22dc777
Huw Griffiths <huw.m.griffiths@gmail.com>
2020-01-28 16:41:29 +1100

    Refactor world testing
    I think using these named subtests are more idiomatic. I did this when I
    was trying to track down a bug and I thought I could isolate
    it to the Tagged method, so there's an extra test included here too.

--- ecs/world_test.go
+++ ecs/world_test.go
@@ -5,66 +5,87 @@ import (
 	"testing"
 )
 
-func TestHasTag(t *testing.T) {
-	t.Run("nonexistant-entity", func(t *testing.T) {
-		mgr := NewWorld()
+func TestWorld(t *testing.T) {
+	t.Run("HasTag", func(t *testing.T) {
+		t.Run("nonexistant-entity", func(t *testing.T) {
+			mgr := NewWorld()
 
-		if mgr.HasTag(123456, "RedHerring") {
-			t.Error("want not tagged for nonexistant entity")
-		}
+			if mgr.HasTag(123456, "RedHerring") {
+				t.Error("want not tagged for nonexistant entity")
+			}
+		})
+
+		t.Run("tagged-not-tagged", func(t *testing.T) {
+			mgr := NewWorld()
+			e := mgr.NewEntity()
+
+			mgr.Tag(e, "Present")
+			mgr.Tag(e, "RedHerring")
+
+			if !mgr.HasTag(e, "Present") {
+				t.Error("want tagged with Present, got not tagged")
+			}
+			if mgr.HasTag(e, "NotPresent") {
+				t.Error("want not tagged with NotPresent, got tagged")
+			}
+
+		})
 	})
-
-	t.Run("tagged-not-tagged", func(t *testing.T) {
+	t.Run("RemoveTag", func(t *testing.T) {
 		mgr := NewWorld()
 		e := mgr.NewEntity()
-
-		mgr.Tag(e, "Present")
+		mgr.Tag(e, "B")
 		mgr.Tag(e, "RedHerring")
 
-		if !mgr.HasTag(e, "Present") {
-			t.Error("want tagged with Present, got not tagged")
-		}
-		if mgr.HasTag(e, "NotPresent") {
-			t.Error("want not tagged with NotPresent, got tagged")
+		e = mgr.NewEntity()
+		mgr.Tag(e, "A")
+		mgr.Tag(e, "B")
+
+		if len(mgr.Tagged("A")) != 1 {
+			t.Errorf("want one Entity tagged with A, got %d", len(mgr.Tagged("A")))
 		}
 
+		mgr.RemoveTag(e, "A")
+		if len(mgr.Tagged("A")) != 0 {
+			t.Errorf("want no Entities tagged with A, got %d", len(mgr.Tagged("A")))
+		}
+
+		if len(mgr.Tagged("B")) != 2 {
+			t.Errorf("want one Entity tagged with B, got %d", len(mgr.Tagged("B")))
+		}
 	})
-}
+	t.Run("ListComponents", func(t *testing.T) {
+		mgr := NewWorld()
+		e := mgr.NewEntity()
+		mgr.AddComponent(e, &Children{})
+		mgr.Tag(e, "Anything")
 
-func TestRemoveTag(t *testing.T) {
-	mgr := NewWorld()
-	e := mgr.NewEntity()
-	mgr.Tag(e, "B")
-	mgr.Tag(e, "RedHerring")
+		got := mgr.ListComponents(e)
 
-	e = mgr.NewEntity()
-	mgr.Tag(e, "A")
-	mgr.Tag(e, "B")
+		want := []string{"Children", "Tags"}
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("want %v, got %v", want, got)
+		}
+	})
 
-	if len(mgr.Tagged("A")) != 1 {
-		t.Errorf("want one Entity tagged with A, got %d", len(mgr.Tagged("A")))
-	}
+	t.Run("Tagged", func(t *testing.T) {
+		mgr := NewWorld()
+		for i := 0; i < 100; i++ {
+			e := mgr.NewEntity()
+			mgr.Tag(e, "prefix-tag")
+			if i%7 == 0 {
+				mgr.Tag(e, "mod7iszero")
+			}
+		}
 
-	mgr.RemoveTag(e, "A")
-	if len(mgr.Tagged("A")) != 0 {
-		t.Errorf("want no Entities tagged with A, got %d", len(mgr.Tagged("A")))
-	}
+		prefixed := len(mgr.Tagged("prefix-tag"))
+		if prefixed != 100 {
+			t.Errorf("want 100 tagged with prefix, got %d", prefixed)
+		}
 
-	if len(mgr.Tagged("B")) != 2 {
-		t.Errorf("want one Entity tagged with B, got %d", len(mgr.Tagged("B")))
-	}
-}
-
-func TestListComponents(t *testing.T) {
-	mgr := NewWorld()
-	e := mgr.NewEntity()
-	mgr.AddComponent(e, &Children{})
-	mgr.Tag(e, "Anything")
-
-	got := mgr.ListComponents(e)
-
-	want := []string{"Children", "Tags"}
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("want %v, got %v", want, got)
-	}
+		mod7ed := len(mgr.Tagged("mod7iszero"))
+		if mod7ed != 15 {
+			t.Errorf("want 15 tagged with mod7, got %d", mod7ed)
+		}
+	})
 }
