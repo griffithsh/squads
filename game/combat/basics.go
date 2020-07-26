3c3ffa79a6b9fc16bbba9fb011508304f268260f
Huw Griffiths <huw.m.griffiths@gmail.com>
2020-07-26 20:48:47 +1000

    combat: Move constant declarations
    I think this is a better name.

--- game/combat/basics.go
+++
@@ -1,6 +0,0 @@
-package combat
-
-const hexagonWingWidth int = 19
-const hexagonBodyWidth int = 34
-const hexagonTileWidth int = hexagonWingWidth + hexagonBodyWidth + hexagonWingWidth
-const hexagonHeight int = 40
---
+++ game/combat/constants.go
@@ -0,0 +1,9 @@
+package combat
+
+// define the size of hexagons used by combat.
+const (
+	hexagonWingWidth int = 19
+	hexagonBodyWidth int = 34
+	hexagonTileWidth int = hexagonWingWidth + hexagonBodyWidth + hexagonWingWidth
+	hexagonHeight    int = 40
+)
